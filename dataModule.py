b7021c6a6193fd3aafd173e08a871ac075c6e6c6
Caner <canerturkmen@msn.com>
2021-04-09 21:27:46 +0300

    dataModule.py updated

--- dataModule.py
+++ dataModule.py
@@ -10,12 +10,12 @@ import random
 class ImageTransform:
     def __init__(self, img_size=256):
         self.transform = {
-            'train': transforms.Compose([
+            "train": transforms.Compose([
                 transforms.Resize((img_size, img_size)),
                 transforms.ToTensor(),
                 transforms.Normalize(mean=[0.5], std=[0.5])
             ]),
-            'test': transforms.Compose([
+            "test": transforms.Compose([
                 transforms.Resize((img_size, img_size)),
                 transforms.ToTensor(),
                 transforms.Normalize(mean=[0.5], std=[0.5])
